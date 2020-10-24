ffe7f7ac83d23f12020e58cd99ee50731f0d059b
AlienDev66 <p.capitango65@gmail.com>
2020-10-24 21:44:21 +0100

    :sparkles: Tempalte Literals.

--- ES6/conceitos/configurandoBabel/main.js
+++ ES6/conceitos/configurandoBabel/main.js
@@ -1,19 +1,4 @@
-// REST
+const nome = "Domingos";
+const idade = 19;
 
-function soma(a, b, ...pararms) {
-  return pararms;
-}
-
-console.log(soma(1, 3, 8, 10, 40));
-
-// SPREAD
-
-const usuario = {
-  nome: "Domingos",
-  idade: 19,
-  empresa: "SNIR",
-};
-
-const usuario2 = { ...usuario, nome: "João" };
-
-console.log(usuario2);
+console.log(`Meu nome é ${nome} e a minha idade é ${idade}`);
