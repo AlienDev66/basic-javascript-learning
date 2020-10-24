4918a87e92e08a4d1f0a0106b12438c82dd92774
AlienDev66 <p.capitango65@gmail.com>
2020-10-24 20:49:36 +0100

    :sparkles: Let e Const.

--- ES6/conceitos/configurandoBabel/bundle.js
+++ ES6/conceitos/configurandoBabel/bundle.js
@@ -1,24 +1,12 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function teste(x) {
+  var y = 2;
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var Matermatica = /*#__PURE__*/function () {
-  function Matermatica() {
-    _classCallCheck(this, Matermatica);
+  if (x > 5) {
+    var _y = 4;
+    console.log(x, _y);
   }
+}
 
-  _createClass(Matermatica, null, [{
-    key: "soma",
-    value: function soma(num1, num2) {
-      return num1 + num2;
-    }
-  }]);
-
-  return Matermatica;
-}();
-
-console.log(Matermatica.soma(84645465465446, 54654651681616161561616));
+teste(10);
