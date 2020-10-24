b8b8e97b90ab57669d84594777ba4e63cc35e4c8
AlienDev66 <p.capitango65@gmail.com>
2020-10-24 20:23:00 +0100

    :sparkles: Conceittos sobre classe.

--- ES6/conceitos/configurandoBabel/main.js
+++ ES6/conceitos/configurandoBabel/main.js
@@ -1,5 +1,8 @@
-alert("Test");
-class Test {
-  metodo() {}
-  outroMetodo() {}
+class Matermatica {
+
+  static soma (num1, num2){
+  return num1 + num2;
+  }
 }
+
+console.log(Matermatica.soma(84645465465446, 54654651681616161561616));
