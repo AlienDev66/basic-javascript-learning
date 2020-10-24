cec5cef5f1265742b5720bc75a6069367549d278
AlienDev66 <p.capitango65@gmail.com>
2020-10-24 21:46:38 +0100

    :sparkles: Object Short sintax.

--- ES6/conceitos/configurandoBabel/bundle.js
+++ ES6/conceitos/configurandoBabel/bundle.js
@@ -2,4 +2,9 @@
 
 var nome = "Domingos";
 var idade = 19;
-console.log("Meu nome \xE9 ".concat(nome, " e a minha idade \xE9 ").concat(idade));
+var usuario = {
+  nome: nome,
+  idade: idade,
+  empresa: "Rocketseat"
+};
+console.log(usuario);
