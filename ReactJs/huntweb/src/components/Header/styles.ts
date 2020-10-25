4b7fe1f9d1c9e01c6da8ae06c82c72413bddc595
AlienDev66 <p.capitango65@gmail.com>
2020-10-26 00:01:02 +0100

    :tada: Começando com a parte web, conectando com a api, listando produtos.

---
+++ ReactJs/huntweb/src/components/Header/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const MainHeader = styled.header`
+  width: 100%;
+  height: 60px;
+  background: #da552f;
+  font-size: 18px;
+  font-weight: bold;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
