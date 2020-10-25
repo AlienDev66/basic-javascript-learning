a29605e7cafebbf0bc624d2db827d0cd0e11e203
AlienDev66 <p.capitango65@gmail.com>
2020-10-25 06:29:13 +0100

    :rocket: Utilizando nodemon.

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  response.send("Hello World");
+  response.send("Hello World 2");
 });
 
 app.listen(3001);
