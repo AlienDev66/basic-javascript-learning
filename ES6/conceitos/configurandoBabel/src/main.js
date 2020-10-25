1e21fd42300d5c3d1754494e7b024bfbd086fa1f
AlienDev66 <p.capitango65@gmail.com>
2020-10-25 05:09:17 +0100

    :sparkles: Render em tela.

--- ES6/conceitos/configurandoBabel/src/main.js
+++ ES6/conceitos/configurandoBabel/src/main.js
@@ -2,6 +2,7 @@ class App {
   constructor() {
     this.repositories = [];
     this.formEl = document.getElementById("repo-form");
+    this.listEl = document.getElementById("repo-list");
 
     this.registerHandler();
   }
@@ -21,7 +22,33 @@ class App {
       html_url: "https://github.com/AlienDev66/InviteYou-frontend",
     });
 
-    console.log(this.repositories);
+    this.render();
+  }
+  render() {
+    this.listEl.innerHTML = "";
+
+    this.repositories.forEach((repo) => {
+      let imgEl = document.createElement("img");
+      imgEl.setAttribute("src", repo.avatar_url);
+
+      let titleEl = document.createElement("strong");
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement("p");
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement("a");
+      linkEl.setAttribute("target", "_blank");
+      linkEl.appendChild(document.createTextNode("Acessar"));
+
+      let listItemEl = document.createElement("li");
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.listEl.appendChild(listItemEl);
+    });
   }
 }
 
