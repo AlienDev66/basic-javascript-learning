f28435f3c4afe50f3d486a233d2b592454a87955
AlienDev66 <p.capitango65@gmail.com>
2020-10-25 20:53:17 +0100

    :sparkles: Criando model de produtos.

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 //INICIANDO O APP
 const app = express();
@@ -10,8 +11,17 @@ mongoose.connect("mongodb://localhost:27017/nodeapi", {
   useNewUrlParser: true,
 });
 
+requireDir("./src/models");
+
+const Product = mongoose.model("Product");
+
 // PRIMEIRA ROTA
 app.get("/", (request, response) => {
+  Product.create({
+    title: "React Native",
+    description: "Build native apps with React",
+    url: "http://github.com/facebook/react-native",
+  });
   response.send("Hello World 2");
 });
 
