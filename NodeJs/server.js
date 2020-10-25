1e8552a35de15e465391ebdaf2aa248f83dd344d
AlienDev66 <p.capitango65@gmail.com>
2020-10-25 21:04:49 +0100

    :sparkles: Reestruturação de arquivos.

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -13,16 +13,7 @@ mongoose.connect("mongodb://localhost:27017/nodeapi", {
 
 requireDir("./src/models");
 
-const Product = mongoose.model("Product");
-
-// PRIMEIRA ROTA
-app.get("/", (request, response) => {
-  Product.create({
-    title: "React Native",
-    description: "Build native apps with React",
-    url: "http://github.com/facebook/react-native",
-  });
-  response.send("Hello World 2");
-});
+//Rotas
+app.use("/api", require("./src/routes"));
 
 app.listen(3001);
