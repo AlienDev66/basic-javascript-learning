749cd43bb3754603875ebaae2dab322702aae22d
AlienDev66 <p.capitango65@gmail.com>
2020-10-25 04:16:25 +0100

    :sparkles: Async Await.

--- ES6/conceitos/configurandoBabel/src/main.js
+++ ES6/conceitos/configurandoBabel/src/main.js
@@ -1,1 +1,14 @@
-alert("Opa");
+const minhaPromise = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("OK");
+    }, 2000);
+  });
+
+async function executaPromise() {
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+}
+
+executaPromise();
