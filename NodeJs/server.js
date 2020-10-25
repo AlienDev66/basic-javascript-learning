04c2885d935531a69ee113623195ab91a64dbd82
AlienDev66 <p.capitango65@gmail.com>
2020-10-25 21:43:29 +0100

    :sparkles: Adicioanando cors.

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
+const cors = require("cors");
 
 //INICIANDO O APP
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // INICIANDO O DB
 mongoose.connect("mongodb://localhost:27017/nodeapi", {
