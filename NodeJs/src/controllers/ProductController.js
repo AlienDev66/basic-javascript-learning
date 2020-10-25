3ac45a2cc9c7c126b3b88498427f5397c9ad0b5a
AlienDev66 <p.capitango65@gmail.com>
2020-10-25 21:17:33 +0100

    :sparkles: Criação de registro.

--- NodeJs/src/controllers/ProductController.js
+++ NodeJs/src/controllers/ProductController.js
@@ -5,7 +5,13 @@ const Product = mongoose.model("Product");
 module.exports = {
   async index(require, response) {
     const products = await Product.find();
-
     return response.json(products);
+  },
+
+  async store(require, response) {
+    // Criação
+    const product = await Product.create(require.body);
+
+    return response.json(product);
   },
 };
