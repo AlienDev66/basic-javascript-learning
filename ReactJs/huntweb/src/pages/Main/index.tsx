45b0232d6f3c01eea715987d4feaa9a1dcb24af9
AlienDev66 <p.capitango65@gmail.com>
2020-10-26 00:42:19 +0100

    :sparkles: Botão de Anterior e próximo.

--- ReactJs/huntweb/src/pages/Main/index.tsx
+++ ReactJs/huntweb/src/pages/Main/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import api from "../../services/api";
-import { Container, ProductList, Article } from "./styles";
+import { Container, ProductList, Article, Actions } from "./styles";
 
 interface ProductProps {
   _id: String;
@@ -8,13 +8,41 @@ interface ProductProps {
   description: String;
   url: String;
 }
+interface productInfoProps {
+  total: number;
+  limit: number;
+  page: number;
+  pages: number;
+}
 
 export default function Main() {
   const [products, setProducts] = useState<ProductProps[]>([]);
+  const [productInfo, setProductInfo] = useState<productInfoProps>();
+  const [page, setPage] = useState(1);
 
-  async function loadProduct() {
-    const response = await api.get("/products");
-    setProducts(response.data.docs);
+  function prevPage() {
+    if (page === 1) return;
+    const pageNumber = page - 1;
+    loadProduct(pageNumber);
+    setPage(pageNumber);
+  }
+
+  function nextPage() {
+    if (page === productInfo?.pages) return;
+    const pageNumber = page + 1;
+    loadProduct(pageNumber);
+    setPage(pageNumber);
+  }
+
+  async function loadProduct(page = 1) {
+    const response = await api.get(`/products?page=${page}`);
+
+    const { docs, ...productInfo } = response.data;
+
+    console.log(productInfo);
+
+    setProducts(docs);
+    setProductInfo(productInfo);
   }
 
   useEffect(() => {
@@ -32,6 +60,14 @@ export default function Main() {
             <a href="">Acessar</a>
           </Article>
         ))}
+        <Actions>
+          <button disabled={page === 1} onClick={prevPage}>
+            Anterior
+          </button>
+          <button disabled={page === productInfo?.pages} onClick={nextPage}>
+            Próximo
+          </button>
+        </Actions>
       </ProductList>
     </Container>
   );
