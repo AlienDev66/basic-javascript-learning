2b455948aafe4043e5c56cae051177ad72f24259
AlienDev66 <p.capitango65@gmail.com>
2020-10-26 00:00:59 +0100

    :tada: Começando com a parte web, conectando com a api, listando produtos.

---
+++ ReactJs/huntweb/src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container, MainHeader } from "./styles";
+
+const Header: React.FC = () => {
+  return (
+    <Container>
+      <MainHeader>JSHunt</MainHeader>
+    </Container>
+  );
+};
+
+export default Header;
