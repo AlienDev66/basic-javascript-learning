867bb5486c63c1fdc4f4b8663a820c2a370bc090
AlienDev66 <p.capitango65@gmail.com>
2020-10-26 00:01:11 +0100

    :tada: Começando com a parte web, conectando com a api, listando produtos.

---
+++ ReactJs/huntweb/src/styles/GlobalStyles.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+*{
+ margin: 0;
+ padding: 0;
+ box-sizing: border-box;
+}
+
+body{ 
+ font-family: Arial, Arial, Helvetica, sans-serif;
+ background: #fafafa;
+ color: #333;
+}
+`;
