2d8d4be83e9733a3ee10633339d140795385c801
AlienDev66 <p.capitango65@gmail.com>
2020-10-25 21:31:09 +0100

    :sparkles: CRUD.

--- NodeJs/src/controllers/ProductController.js
+++ NodeJs/src/controllers/ProductController.js
@@ -3,15 +3,32 @@ const mongoose = require("mongoose");
 const Product = mongoose.model("Product");
 
 module.exports = {
-  async index(require, response) {
+  async index(request, response) {
     const products = await Product.find();
     return response.json(products);
   },
 
-  async store(require, response) {
-    // Criação
-    const product = await Product.create(require.body);
-
+  async show(request, response) {
+    const product = await Product.findById(request.params.id);
     return response.json(product);
+  },
+
+  async store(request, response) {
+    // Criação
+    const product = await Product.create(request.body);
+    return response.json(product);
+  },
+
+  async update(request, response) {
+    const product = await Product.findOneAndUpdate(
+      request.params.id,
+      request.body,
+      { new: true }
+    );
+    return response.json(product);
+  },
+  async destroy(request, response) {
+    await Product.findOneAndDelete(request.params.id);
+    return response.send("Deletado");
   },
 };
