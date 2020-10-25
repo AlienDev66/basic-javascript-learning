6658b2a5050d8f58053d996fa70e0377f6b474ae
AlienDev66 <p.capitango65@gmail.com>
2020-10-25 20:09:36 +0100

    :sparkles: Conectando com o banco de dados.

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+//INICIANDO O APP
 const app = express();
 
+// INICIANDO O DB
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+// PRIMEIRA ROTA
 app.get("/", (request, response) => {
   response.send("Hello World 2");
 });
