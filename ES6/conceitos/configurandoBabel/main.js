deab4a0ceac8834202b11409d0aad45da3568d46
AlienDev66 <p.capitango65@gmail.com>
2020-10-25 03:31:28 +0100

    :sparkles: Import e Export.

--- ES6/conceitos/configurandoBabel/main.js
+++ ES6/conceitos/configurandoBabel/main.js
@@ -1,3 +1,5 @@
-import { soma } from "./funcoes";
+// import { soma, sub } from "./funcoes";
+import * as funcoes from "./funcoes";
 
-console.log(soma(1, 2));
+console.log(funcoes.soma(1, 2));
+// console.log(sub(10, 22));
