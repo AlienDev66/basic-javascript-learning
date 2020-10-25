3bc3b5e5ede53c299cee177a8bf3ebe66429929e
AlienDev66 <p.capitango65@gmail.com>
2020-10-25 04:29:58 +0100

    :sparkles: Configurando Axios.

--- ES6/conceitos/configurandoBabel/src/main.js
+++ ES6/conceitos/configurandoBabel/src/main.js
@@ -1,14 +1,16 @@
-const minhaPromise = () =>
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("OK");
-    }, 2000);
-  });
+import axios from "axios";
 
-async function executaPromise() {
-  console.log(await minhaPromise());
-  console.log(await minhaPromise());
-  console.log(await minhaPromise());
+class Api {
+  static async getUserInfo(username) {
+    try {
+      const response = await axios.get(
+        `https://api.github.com/users/${username}`
+      );
+      console.log(response);
+    } catch (err) {
+      console.warn("Erro na Api");
+    }
+  }
 }
 
-executaPromise();
+Api.getUserInfo("AlienDev66");
