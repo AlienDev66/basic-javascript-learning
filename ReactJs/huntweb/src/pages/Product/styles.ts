793286926194f61fe52d7da9b43bf21d013c9cbf
AlienDev66 <p.capitango65@gmail.com>
2020-10-26 22:07:31 +0100

    :rocket: Finish huntweb.

---
+++ ReactJs/huntweb/src/pages/Product/styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const ProductInfo = styled.div`
+  max-width: 700px;
+  margin: 20px auto 0;
+  padding: 20px;
+  background: #fff;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+
+  h1 {
+    font-size: 32px;
+  }
+  p {
+    color: #666;
+    line-height: 24px;
+    margin-top: 5px;
+  }
+  a {
+    color: #069;
+  }
+`;
