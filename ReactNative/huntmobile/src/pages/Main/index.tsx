491bebfa535ddbc000c6fb589465b0f0f06353d8
AlienDev66 <p.capitango65@gmail.com>
2020-10-28 21:44:12 +0100

    :sparkles: Criaçaõ de roitas.

---
+++ ReactNative/huntmobile/src/pages/Main/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from "react-native";
+
+import { Container } from "./styles";
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+      <Text>Main</Text>
+    </Container>
+  );
+};
+
+export default Main;
